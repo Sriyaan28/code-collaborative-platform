d1de8cc96e4d52d2467117d5028f5ff960b9e112
Sriyaan28 <sriyaanreddy4320@gmail.com>
2026-05-13 18:26:41 +0530

    fixed minor errors

--- backend/APIs/TestAPI.js
+++ backend/APIs/TestAPI.js
@@ -1,18 +1,29 @@
-// a route to get demo data to test
-
-import exp from 'express';
-import { UserModel } from '../models/UserModel.js';
+import exp from 'express'
+import { UserModel } from '../models/UserModel.js'
 
 export const testApp = exp.Router()
 
-// route for testing
 testApp.get('/demo', async (req, res) => {
-    // fetch all profiles from db
-    const users = await UserModel.find();
-    if (!users) {
-        return res.status(404).json({ message: "No users found" })
+
+    try {
+
+        const users = await UserModel.find()
+
+        res.status(200).json({
+            message: "Test API is working",
+            data: users
+        })
+
     }
-    console.log(users)
-    res.status(200).json({ message: "Test API is working", data: users })
-    console.log("test API is working")
+    catch (err) {
+
+        console.log(err)
+
+        res.status(500).json({
+            message: "Database Error",
+            error: err.message
+        })
+
+    }
+
 })
